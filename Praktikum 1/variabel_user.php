89b7917792734ddd872bc0eb01cf5f265d067fc6
rakaduta08 <147326150+rakaduta08@users.noreply.github.com>
2024-02-28 21:16:27 +0700

    Add files via upload
    Praktikum 1

---
+++ Praktikum 1/variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "Raka Duta Fahrizki";
+$umur =  18;
+$berat = 75;
+
+echo "nama : $nama";
+echo "<br/>umur : $umur tahun";
+echo "<br/>berat : $berat KG";
+echo "<br/>Hello : $nama Apakabar";
+
